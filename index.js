3e4fbe28b411f523226626b65f365837a88541f2
JustMrPhoenix <mrphoenixx@mail.ru>
2018-11-03 12:54:32 +0400

    Fixed gitignore, fixed index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,4 +12,4 @@ client.on('message', msg => {
   }
 });
 
-client.login('token');
+client.login(config.apiToken);
