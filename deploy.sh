4eaea1776ab12b06bf84020b987ff573453c53ba
Anton Istomin <istomanton@gmail.com>
2019-11-25 21:57:03 +0500

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+set -e
+
+argc=$#
+
+if [ $argc -eq 0 ]; then
+  user="$USER"
+else
+  user="$1"
+fi
+
+
+docker build -t refruity/ezbot:master .
+docker push refruity/ezbot:master
+ssh "$user"@89.178.174.96 "cd /srv/docker/ezbot && docker-compose pull && docker-compose up -d ezbot"
