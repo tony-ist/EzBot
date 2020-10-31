1eb6deb40ce11349eb79456e4b9a0a79200e1e29
JustMrPhoenix <mrphoenix@protonmail.com>
2020-10-31 21:03:52 +0400

    Update package.json with new repo links. Update github workflow. Lint eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
-const OFF = 0, WARN = 1, ERROR = 2
+// eslint-disable-next-line no-unused-vars
+const OFF = 0; const WARN = 1; const ERROR = 2
 
 module.exports = {
-  "extends": "standard",
-  "rules": {
-    "no-var": ERROR,
-    "no-redeclare": ERROR,
-    "space-before-function-paren": ["error", {
-      "anonymous": "never",
-      "named": "never",
-      "asyncArrow": "always"
+  'extends': 'standard',
+  'rules': {
+    'no-var': ERROR,
+    'no-redeclare': ERROR,
+    'space-before-function-paren': ['error', {
+      'anonymous': 'never',
+      'named': 'never',
+      'asyncArrow': 'always'
     }],
-    "array-bracket-spacing": ["error", "never"]
+    'array-bracket-spacing': ['error', 'never']
   }
-};
+}
