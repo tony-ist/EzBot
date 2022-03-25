b9bb2fa24284a292a9f6867c7fdb83475ddf8d0e
Anton Istomin <istomanton@gmail.com>
2022-03-25 16:06:09 +0300

    Update REST to v10

--- src/commands/deploy-commands.js
+++ src/commands/deploy-commands.js
@@ -3,7 +3,7 @@ const { Routes } = require('discord-api-types/v10')
 const { clientId, guildId, discordApiToken } = require('../../config.json')
 const { commandList } = require('./command-list')
 
-const rest = new REST({ version: '9' }).setToken(discordApiToken)
+const rest = new REST({ version: '10' }).setToken(discordApiToken)
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandList })
   .then(() => console.log('Successfully registered application commands.'))
