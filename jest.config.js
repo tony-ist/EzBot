abda86511db407eafe0682fe9651a81bcb52f8a6
a.plesovskikh <a.plesovskikh@tinkoff.ru>
2022-03-25 17:20:47 +0300

    Clean up test setup

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
-const path = require('path')
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // globals: {
-  //   'ts-jest': {
-  //     tsconfig: require('./tsconfig.json').compilerOptions,
-  //   },
-  // },
 }
